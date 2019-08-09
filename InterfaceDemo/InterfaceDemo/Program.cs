e5dd5f1807925adc36dee8aa7b31f558b787c59d
JayarajRepo <53886517+JayarajRepo@users.noreply.github.com>
2019-08-09 08:23:25 +0530

    Interface demo

---
+++ InterfaceDemo/InterfaceDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface I1
+    {
+         void Display();
+    }
+    interface I2
+    {
+        void Display();
+    }
+
+    class MyClass :I1,I2
+    {
+        void I1.Display()
+        {
+            Console.WriteLine("I1 Display");
+        }
+        void I2.Display()
+        {
+            Console.WriteLine("I2 Display");
+        }
+         
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            I1 myClass = new MyClass();
+            myClass.Display();
+           // myClass.Display1();
+
+        }
+    }
+}
