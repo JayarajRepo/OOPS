b4a7762e18c8a508f210ddb6f5bcbd2173e870a3
JayarajRepo <53886517+JayarajRepo@users.noreply.github.com>
2019-08-20 08:44:59 +0530

    Namespace created for sample
    Namespace created for sample

---
+++ Namespace/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyEmployee
+{
+    class Employee
+    {
+        public void EmployeeName()
+        {
+            Console.WriteLine("Empoloyee from MyEmployee NS");
+        }
+    }
+}
---
+++ Namespace/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//using NS1;
+using MyEmployee;
+namespace Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            NS1.C1 cls = new NS1.C1();
+            cls.Method();
+            Employee emp = new Employee();
+            emp.EmployeeName();
+        }
+    }
+    namespace NS1
+    {
+        class C1
+        {
+            public void Method()
+            {
+                Console.WriteLine("Method from NS1");
+            }
+        }
+    }
+
+}
